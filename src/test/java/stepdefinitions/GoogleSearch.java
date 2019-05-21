9887e17a08819c9dc23aac507288ebe2f08da24c
David Ageba <david.ageba@trurating.com>
2019-05-21 22:26:05 +0100

    fix merger issues

--- src/test/java/stepdefinitions/GoogleSearch.java
+++ src/test/java/stepdefinitions/GoogleSearch.java
@@ -4,33 +4,36 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GoogleSearch {
-    WebDriver driver = null;
+    public static WebDriver driver = null;
     @Given("^I am on google search page$")
-    public void i_am_on_google_search_page() throws Throwable {
+    public static void i_am_on_google_search_page() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        System.setProperty("user.dir", System.getProperty("\\driver\\chromedriver.exe"));
-        // System.setProperty("webdriver.chrome.driver","C:\\Users\\DavidAgeba\\JATTrainingOne\\ACUKJATTrainingOne\\driver\\chromedriver.exe");
+       // System.setProperty("user.dir", System.getProperty("\\driver\\chromedriver.exe"));
+         System.setProperty("webdriver.chrome.driver", "C:\\Users\\DavidAgeba\\JATTrainingOne\\ACUKJATTrainingOne\\driver\\chromedriver.exe");
          WebDriver driver = new ChromeDriver();
         // System.setProperty("webdriver.firefox.marionette","C:\\Users\\DavidAgeba\\JATTrainingOne\\ACUKJATTrainingOne\\driver\\geckodriver.exe");
         // WebDriver driver = new FirefoxDriver();
-         driver.get ("https://www.googlo.co.uk");
+         driver.get ("https://www.google.co.uk");
     }
 
     @When("^I search for \"([^\"]*)\"$")
-    public void i_search_for(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println( "Second step" );
+    public void i_search_for(String arg1)  {
+        WebElement search = driver.findElement(By.name("q"));
+        search.sendKeys(arg1);
+        search.submit();
     }
 
-    @Then("^the first line on the result page should contain \"([^\"]*)\"$")
-    public void the_first_line_on_the_result_page_should_contain(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println( "Third step" );
+    @Then("^the page title should contain \"([^\"]*)\"$")
+    public void the_first_line_on_the_result_page_should_contain(String arg1)   {
+        driver.getTitle().contains(arg1);
+        driver.quit();
 
     }
 
